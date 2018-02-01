ec9aaf57379b4d42c024b1a28f3abe5e93112ae6
jaumard <Jimmy.Aumard@nestle.com>
2018-02-01 17:45:08 +0100

    try docker file

--- drivers/voice.js
+++ drivers/voice.js
@@ -3,31 +3,22 @@
 const Driver = require('lisa-plugin').Driver
 
 module.exports = class LISAVoiceDriver extends Driver {
+  _handleError(error) {
+    switch (error.errorCode) {
+      case this.mdns.kDNSServiceErr_Unknown:
+        this.log.warn(error);
+        setTimeout(this._createBrowser, 5000);
+        break;
+      default:
+        this.log.error(error);
+        throw error;
+    }
+  }
+
   init() {
     this.devices = {}
     // Make it work on raspberry pi by forcing ipv4
-    const sequence = [
-      this.lisa.mdns.rst.DNSServiceResolve(), 'DNSServiceGetAddrInfo' in this.lisa.mdns.dns_sd ?
-        this.lisa.mdns.rst.DNSServiceGetAddrInfo() : this.lisa.mdns.rst.getaddrinfo({ families: [4] }),
-      this.lisa.mdns.rst.makeAddressesUnique()
-    ]
-
-    this.browser = this.lisa.mdns.createBrowser(this.lisa.mdns.tcp('http'), { resolverSequence: sequence })
-    this.browser.on('serviceUp', service => {
-      if (service.name.indexOf('lisaVoiceCommand') !== -1) {
-        this.devices[service.txtRecord.identifier] = service
-      }
-    })
-    this.browser.on('serviceDown', service => {
-      if (service.name.indexOf('lisaVoiceCommand') !== -1) {
-        for (let identifier in this.devices) {
-          if (this.devices[identifier].name === service.name) {
-            delete this.devices[identifier]
-          }
-        }
-      }
-    })
-    this.browser.start()
+    this._createBrowser()
     return Promise.resolve()
   }
 
@@ -100,5 +91,35 @@ module.exports = class LISAVoiceDriver extends Driver {
   unload() {
     this.browser.stop()
     return Promise.resolve()
+  }
+
+  _createBrowser() {
+    const sequence = [
+      this.lisa.mdns.rst.DNSServiceResolve(), 'DNSServiceGetAddrInfo' in this.lisa.mdns.dns_sd ?
+        this.lisa.mdns.rst.DNSServiceGetAddrInfo() : this.lisa.mdns.rst.getaddrinfo({ families: [4] }),
+      this.lisa.mdns.rst.makeAddressesUnique()
+    ]
+
+    try {
+      this.browser = this.lisa.mdns.createBrowser(this.lisa.mdns.tcp('http'), { resolverSequence: sequence })
+      this.browser.on('error', this._handleError);
+      this.browser.on('serviceUp', service => {
+        if (service.name.indexOf('lisaVoiceCommand') !== -1) {
+          this.devices[service.txtRecord.identifier] = service
+        }
+      })
+      this.browser.on('serviceDown', service => {
+        if (service.name.indexOf('lisaVoiceCommand') !== -1) {
+          for (let identifier in this.devices) {
+            if (this.devices[identifier].name === service.name) {
+              delete this.devices[identifier]
+            }
+          }
+        }
+      })
+      this.browser.start()
+    } catch (err) {
+      this._handleError(err)
+    }
   }
 }
